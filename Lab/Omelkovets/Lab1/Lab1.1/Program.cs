f247b9249af633e0ffb95ef4e867ee823aa17cfd
Archerusoff <114077293+Archerusoff@users.noreply.github.com>
2022-09-22 14:07:02 +0300

    Lab1

---
+++ Lab/Omelkovets/Lab1/Lab1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Прив1т Св1т");
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();
+        }
+    }
+}
