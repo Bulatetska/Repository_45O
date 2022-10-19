b7f703d225080bec0f6aa4b9c8db8925664f1b59
AlinaPoplavska <114151843+AlinaPoplavska@users.noreply.github.com>
2022-10-19 17:30:08 +0300

    Lab1.3

---
+++ Lab/Poplavska/Laba1/Lab1.3/Lab1.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+            System.Console.WriteLine("a = " + a + ", b =  " + b);
+            int result = a + b;
+            System.Console.WriteLine("Додавання, a + b = " + result);
+            result = a * b;
+            System.Console.WriteLine("Множення, a * b = " + result + " a  i b - цiлi числа, результат - теж");
+            double resultDouble = a / b;
+            System.Console.WriteLine("Дiлення, a / b = " + resultDouble + " помилка...");
+            double aDouble = 5;
+            resultDouble = aDouble / b;
+            System.Console.WriteLine("Дiлення, a / b= " + resultDouble);
+            System.Console.ReadLine();
+        }
+    }
+}
