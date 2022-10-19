4f13ef35158e177d2426a721d319bbbae2f380bb
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-10-19 17:08:47 +0300

    Lab1.1

---
+++ Lab/Kurnosov/Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello world!");
+            System.Console.ReadLine();
+        }
+    }
+}
