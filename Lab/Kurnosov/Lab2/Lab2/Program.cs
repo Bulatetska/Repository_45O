5da2c0db7561b15210e71b80554ed892b662de77
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-10-25 14:33:31 +0300

    Lab2

---
+++ Lab/Kurnosov/Lab2/Lab2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool boolVariable = true;
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = true; --- Істина!");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = falce; --- Хибно!");
+            }
+            System.Console.WriteLine();
+
+            boolVariable = false;
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = falce; --- Істина!");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = falce; --- Хибно!");
+            }
+
+            boolVariable = 2 < 4;
+            if (boolVariable)
+            {
+                System.Console.WriteLine("boolVariable = 2 < 4; --- Істина!");
+            }
+            else
+            {
+                System.Console.WriteLine("boolVariable = 2 < 4; --- Хибно!");
+            }
+
+            if (10 != 100)
+            {
+                System.Console.WriteLine("10 != 100! --- Різні числа!");
+            }
+            System.Console.WriteLine();
+            System.Console.ReadLine();
+        }
+    }
+}
