d52da3d4b09fbfcf241d28151143cff8e5d7010e
AlinaPoplavska <114151843+AlinaPoplavska@users.noreply.github.com>
2022-11-02 18:53:25 +0200

    Lab3.2

--- Lab/Poplavska/Lab3/Lab3.2/Lab3.2/Program.cs
+++ Lab/Poplavska/Lab3/Lab3.2/Lab3.2/Program.cs
@@ -8,8 +8,28 @@ namespace Lab3._2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static string CreateFio(string name, string surname, string pobatkovi)
         {
+            string fio = surname + " " + name + " " + pobatkovi;
+            return fio;
+        }
+        public static string CreateFioInitials(string surname, string name, string pobatkovi)
+        {
+            string fio = surname + " " + name.Substring(0, 1) + ". " + pobatkovi.Substring(0, 1) + ".";
+
+            return fio;
+        }
+        static void Main()
+        {
+
+            string surname = "Шевченко";
+            string name = "Тарас";
+            string pobatkovi = "Григорович";
+
+            System.Console.WriteLine(CreateFio(surname, name, pobatkovi));
+            System.Console.WriteLine(CreateFioInitials(surname, name, pobatkovi));
+            Console.ReadLine();
         }
     }
-}
+  }
+
