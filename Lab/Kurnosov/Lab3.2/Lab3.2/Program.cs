79b53b15d50bcb2250e93f0f689d71763deb2a55
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-02 18:57:32 +0200

    Lab3.2

---
+++ Lab/Kurnosov/Lab3.2/Lab3.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    public static string CreateFio(string surname, string name, string otchestvo)
+    {
+        string fio = surname + " " + name + " " + otchestvo + " ";
+        return fio;
+    }
+    public static string CreateFioShort(string surname, string name, string otchestvo)
+    {
+        string fio = surname + " " + name.Substring(0, 1) + ". " + otchestvo.Substring(0, 1) + ".";
+        return fio;
+    }
+    static void Main()
+    {
+        string surname = "Лавкрафт";
+        string name = "Говард";
+        string otchestvo = "Філіпс";
+        Console.WriteLine(CreateFio(surname, name, otchestvo));
+        Console.WriteLine(CreateFioShort(surname, name, otchestvo));
+        Console.ReadLine();
+    }
+}
