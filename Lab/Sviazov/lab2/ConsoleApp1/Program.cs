d977a19f3e8701e1d66ed21edc79acb9db159ea5
FilnarQ <114151772+ArtemSviiazov@users.noreply.github.com>
2022-11-02 17:54:41 +0200

    lab(2.2)

---
+++ Lab/Sviazov/lab2/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double discriminant = (b * b) - (4 * a * c);
+
+            double x1, x2;
+            if (discriminant < 0)
+            {
+                Console.WriteLine("None root");
+            }
+            else
+            {
+
+                x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                Console.WriteLine("x1={0}, x2={1}", x1, x2);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
