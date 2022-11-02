c36c0f357d1d80752c8d2fbc75a73a173282919b
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-02 18:37:48 +0200

    Lab3.1

---
+++ Lab/Kurnosov/Lab3.1/Lab3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 101; i++)
+        {
+            if (i % 15 == 0)
+            {
+                Console.WriteLine("Кратне трьом і п'яти");
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("Кратне трьом");
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                Console.WriteLine("Кратне п'яти");
+                continue;
+            }
+            Console.WriteLine("i={0}", i);
+        }
+        Console.ReadLine();
+    }
+}
