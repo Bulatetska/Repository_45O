afcdf9f03e86573fb883d66ab1abc72aaa2a8a9c
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-14 16:58:56 +0200

    Lab4.6

---
+++ Lab/Kurnosov/Lab4.6/Lab4.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rand = new Random();
+            for (int x = 0; x < arr.Length; x++)
+            {
+                arr[x] = rand.Next(10);
+                Console.WriteLine(arr[x]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Сума елементів=" + arr.Sum());
+            Console.ReadLine();
+        }
+    }
+}
