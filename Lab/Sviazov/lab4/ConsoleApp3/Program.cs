0ce2adebad5dccfecc4cdc0dcf4bbdf2efa2c536
FilnarQ <114151772+ArtemSviiazov@users.noreply.github.com>
2022-11-14 17:11:29 +0200

    Lab4.6

---
+++ Lab/Sviazov/lab4/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            Random rand = new Random();
+
+            for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = rand.Next(10);
+                Console.WriteLine( myArray[x]);
+                
+            }
+            Console.WriteLine("Сума елемент1в массиву = " + myArray.Sum());
+            Console.ReadLine();
+        }
+    }
+}
