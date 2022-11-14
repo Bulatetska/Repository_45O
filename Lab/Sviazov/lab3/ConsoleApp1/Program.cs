3deb0715a1e6de1a659fbbd8b17d80cd47f36db6
FilnarQ <114151772+ArtemSviiazov@users.noreply.github.com>
2022-11-14 16:40:01 +0200

    Lab3.2

--- Lab/Sviazov/lab3/ConsoleApp1/Program.cs
+++ Lab/Sviazov/lab3/ConsoleApp1/Program.cs
@@ -7,32 +7,13 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string CreateFio(string surname, string name, string otchestvo)
+
+
         {
-            for (int i = 1; i < 101; i++)
-            {
-                if (i % 15 == 0)
-                {
-                    Console.WriteLine("krante 3 i 5");
-                    continue;
-                }
-                if (i % 3 == 0)
-                {
-                    Console.WriteLine("kratne 3");
-                    continue;
-                }
-
-                if (i % 5 == 0)
-                {
-                    Console.WriteLine("kratne 5");
-                    continue;
-                }
-
-
-                Console.WriteLine(i);
-
+            string fio = surname + " " + name + " " + otchestvo;
+                return fio;
             }
-            Console.ReadLine();
-        }
+        
     }
 }
---
+++ Lab/Sviazov/lab4/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+
+        {
+            internal class Program
+        {
+            public static string CreateFio(string name, string surname, string otchestvo)
+            {
+                string fio = surname + " " + name + " " + otchestvo;
+                return fio;
+            }
+            public static string CreateFioInitials(string surname, string name, string otchestvo)
+            {
+                string fio = surname + " " + name.Substring(0, 1) + ". " + otchestvo.Substring(0, 1) + ".";
+
+                return fio;
+            }
+            static void Main()
+            {
+
+                string surname = "Св1язов";
+                string name = "Артем";
+                string otchestvo = "Анатол1йович";
+
+                Console.WriteLine(CreateFio(surname,name,otchestvo));
+                Console.WriteLine(CreateFioInitials(surname,name,otchestvo));
+            string surname1 = "Ярослав";
+            string name1 = "Видринський";
+            string otchestvo1 = "Борисович";
+
+            Console.WriteLine(CreateFio(surname1, name1, otchestvo1));
+            Console.WriteLine(CreateFioInitials(surname1, name1, otchestvo1));
+
+
+            Console.ReadLine();
+            }
+        }
+    }
+    
+
