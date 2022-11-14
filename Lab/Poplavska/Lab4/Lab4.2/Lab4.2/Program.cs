f7794e4218671f837301203b247e87e81ddd6947
AlinaPoplavska <114151843+AlinaPoplavska@users.noreply.github.com>
2022-11-14 17:41:13 +0200

    Lab4.2

---
+++ Lab/Poplavska/Lab4/Lab4.2/Lab4.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int s = int.MaxValue;
+            
+         //   int max = matrix.Max<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" " + matrix[i, j]);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Змiнений масив");
+
+
+          /*  for (int j = 0; j < 3; j++)
+            {
+                if (matrix[i,j] == int.MaxValue) { 
+                    
+                }
+            }
+          */
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matrix[i, j] == )
+                    {
+                        Console.WriteLine("gfd");
+                    }
+                        Console.Write(" " + matrix[i, j]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
