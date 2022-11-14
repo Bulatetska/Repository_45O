44d768cd8f8350a7cf9329c09f5bad53ea40453c
AlinaPoplavska <114151843+AlinaPoplavska@users.noreply.github.com>
2022-11-14 16:18:04 +0200

    Lab4.1

---
+++ Lab/Poplavska/Lab4/Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10] ;
+            Random rand = new Random();
+
+            for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = rand.Next(20);
+                Console.WriteLine("Значення елементу масива " + x + " = " + myArray[x]);
+             }
+            int Max = myArray.Max<int>();
+            Console.WriteLine("Максимальне число = "+ Max);
+            Console.ReadLine ();
+        }
+    }
+}
