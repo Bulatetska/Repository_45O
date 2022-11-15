602345bd891375df672076564ca9318e50f2bc4f
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-15 17:21:39 +0200

    Lab4.1

---
+++ Lab/Kurnosov/Lab4.1/Lab4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random rand = new Random();
+            for (int x = 0; x < arr.Length; x++)
+            {
+                arr[x] = rand.Next(20);
+                System.Console.WriteLine("№" + x + " " + arr[x]);
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Максимальне значення = " + arr.Max());
+            System.Console.WriteLine();
+            System.Console.WriteLine("Кількість повторень - " + arr.Count(n => n == arr.Max()));
+            System.Console.ReadLine();
+        }
+    }
+}
