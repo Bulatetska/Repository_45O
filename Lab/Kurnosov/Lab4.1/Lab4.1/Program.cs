43dd28d26719330515b62d19659cb34e8ecd06f7
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-15 20:04:52 +0200

    Update Program.cs

--- Lab/Kurnosov/Lab4.1/Lab4.1/Program.cs
+++ Lab/Kurnosov/Lab4.1/Lab4.1/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Lab4._1
-{
     class Program
     {
         static void Main(string[] args)
@@ -23,4 +21,3 @@ namespace Lab4._1
             System.Console.ReadLine();
         }
     }
-}
