a94df1e19d164a8b3191e6cc16edd1cff52fce00
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-15 20:45:03 +0200

    Lab4.3

---
+++ Lab/Kurnosov/Lab4.3/Lab4.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 3, 3, 2, 2, 0, 0, 1, 1 };
+        int plusCouple = 0;
+        int zeroCouple = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+
+            System.Console.Write(arr[i] + " ");
+            if (i + 1 < arr.Length)
+            {
+                if (arr[i] > 0 && arr[i + 1] > 1)
+                {
+                    plusCouple++;
+                }
+
+                if (arr[i] == 0 && arr[i + 1] > 0)
+                {
+                    zeroCouple++;
+                }
+            }
+
+        }
+        System.Console.WriteLine();
+        System.Console.WriteLine("Plus couples : " + plusCouple);
+        System.Console.WriteLine("Zero couples: " + zeroCouple);
+        System.Console.ReadLine();
+    }
+}
+
