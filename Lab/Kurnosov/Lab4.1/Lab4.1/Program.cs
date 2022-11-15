464650e6b9161fe7edc5ad1033241783b3894e39
KurnosovVolodymyr <114151839+KurnosovVolodymyr@users.noreply.github.com>
2022-11-15 20:50:35 +0200

    Lab4.1

--- Lab/Kurnosov/Lab4.1/Lab4.1/Program.cs
+++ Lab/Kurnosov/Lab4.1/Lab4.1/Program.cs
@@ -3,36 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+namespace Lab4._1
+{
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] arr = { { 4, 5, 1 }, { 4, 5, 1 }, { 4, 5, 1 } };
-            for (int i = 0; i < 3; i++)
+            int[] arr = new int[10];
+            Random rand = new Random();
+            for (int x = 0; x < arr.Length; x++)
             {
-                for (int j = 0; j < 3; j++)
-                {
-                    System.Console.Write(arr[i, j]);
-                    if (arr[i, j] == arr.Max())
-                    {
-                        arr[i, j] = 0;
-                    }
-                }
-                System.Console.WriteLine();
+                arr[x] = rand.Next(20);
+                System.Console.WriteLine("№" + x + " " + arr[x]);
             }
-
-            System.Console.WriteLine("---");
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    System.Console.Write(arr[i, j]);
-
-                }
-                System.Console.WriteLine();
-            }
-
-            System.Console.WriteLine("==========");
+            System.Console.WriteLine();
+            System.Console.WriteLine("Максимальне значення = " + arr.Max());
+            System.Console.WriteLine();
+            System.Console.WriteLine("Кількість повторень - " + arr.Count(n => n == arr.Max()));
+            System.Console.ReadLine();
         }
     }
+}
