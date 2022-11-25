17936aabdc5f20e7ba56d8d844b2d93a0eab6366
AlinaPoplavska <114151843+AlinaPoplavska@users.noreply.github.com>
2022-11-26 01:57:03 +0200

    Lab4.3

---
+++ Lab/Poplavska/Lab4/Lab4.3/Lab4.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 0, 3, 0, 0, -1, 5, 4, 0, 0, 0, 1, 3, 4, 0, 0, 1, -1 };
+            int zero=0;
+            int plus=0;
+
+            for (int i = 0; i < num.Length; i++) {
+            
+            Console.Write(num[i]+ " ");
+            if(i+1<num.Length){
+                if(num[i]>0&& num[i+1]>0){
+                    plus++;
+                }
+                if(num[i]==0 && num[i+1]==0){
+                    zero++;
+                }
+            }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Кiлькiсть сусiдств двох додатнiх чисел =  " + plus);
+            Console.WriteLine("Кiлькiсть сусiдств двох нульових елементiв =  " + zero);
+            Console.ReadLine();
+        }
+    }
+}
