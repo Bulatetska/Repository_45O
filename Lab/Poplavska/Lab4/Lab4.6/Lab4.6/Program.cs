05f0e5e72864e22746125e1626be800832c99c6b
AlinaPoplavska <114151843+AlinaPoplavska@users.noreply.github.com>
2022-11-26 03:04:12 +0200

    Lab4.6

---
+++ Lab/Poplavska/Lab4/Lab4.6/Lab4.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5];
+            Random rand = new Random();
+
+            for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = rand.Next(10);
+                Console.WriteLine("Значення елементу масива " + x + " = " + myArray[x]);
+            }
+             int sum = myArray.Cast<int>().Sum();
+            Console.WriteLine("Сума = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
