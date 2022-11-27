56f9c9c38e2536fd55664d8d34235c20456ea417
ArtemSviiazov <114151772+ArtemSviiazov@users.noreply.github.com>
2022-11-27 03:24:43 +0200

    lab 4.2

--- Lab/Sviazov/Lab2/Lab2/Program.cs
+++ Lab/Sviazov/Lab2/Lab2/Program.cs
@@ -9,42 +9,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            bool boolVariable = true;
-            if (boolVariable)
+            for (int i = 1; i < 101; i++)
             {
-                System.Console.WriteLine("boolVariable = true; --- Істина!");
-            }
-            else
-            {
-                System.Console.WriteLine("boolVariable = false; --- Хибно!");
-            }
-            System.Console.WriteLine();
+                if (i % 3 == 0)
+                    Console.WriteLine("kratne 3");
+               
 
-            boolVariable = false;
-            if (boolVariable)
-            {
-                System.Console.WriteLine("boolVariable = false; --- Істина!");
-            }
-            else
-            {
-                System.Console.WriteLine("boolVariable = false; --- Хибно!");
-            }
-            boolVariable = 2 < 4;
-            if (boolVariable)
-            {
-                System.Console.WriteLine("boolVariable = 2 < 4; Істина!");
-            }
-            else
-            {
-                System.Console.WriteLine("boolVariable = 2 < 4; Хибно!");
-            }
-            if (10 != 100)
-            {
-                System.Console.WriteLine("10 != 100! Різні числа");
-            }
-            System.Console.WriteLine();
-            System.Console.ReadLine();
+                if (i % 5 == 0)
+                    Console.WriteLine("kratne 5");
 
+
+
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ Lab/Sviazov/lab 4/Lab4.2/Lab4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = { { 5, 2, 5 }, { 2, 4, 1 }, { 5, 3, 3 } };
+            int maxNum = matrix.Cast<int>().Max();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    if (matrix[i, j] == maxNum)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    if (matrix[i, j] == maxNum)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
