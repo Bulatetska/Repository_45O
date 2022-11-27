160482e52a50e2bebf23479d729243b9c6fca1af
ArtemSviiazov <114151772+ArtemSviiazov@users.noreply.github.com>
2022-11-27 04:51:32 +0200

    lab 4.1

---
+++ Lab/Sviazov/lab 4/Lab 4.1/Lab 4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[10];
+            Random rand = new Random();
+
+            for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = rand.Next(20);
+                Console.WriteLine("Елемент массиву " + x + " = " + myArray[x]);
+              
+            }
+            Console.WriteLine();
+
+            int Max = myArray.Max<int>();
+            int pos = Array.IndexOf(myArray, Max);
+            Array.Sort(myArray);
+            int maxValue = myArray[myArray.Length - 1];
+            Console.WriteLine("Максимальне значення = " + maxValue);
+            Console.WriteLine();
+            Console.WriteLine("1ндекс елементу = " + pos + ", " +  "К1льк1сть повторень = " + myArray.Count(n => n == Max));
+            
+            Console.ReadLine();
+        }
+    }
+}
